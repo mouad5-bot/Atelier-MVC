0fcea1b0572e7fbb1d7d14f9a78d31b918e319ac
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-01-08 16:40:19 +0100

    done with design | FrontEnd!

--- index.php
+++ index.php
@@ -5,8 +5,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>convert | money</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="card-global">
+        <div class="card text-bg-dark mb-3">   
+        <div class="card-header">
+            <h3 class="mt-3 mb-4">Covert | Money</h3>
+        </div> 
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-3">
+                            <label for="">MAD :</label>
+                        </div>
+                        <div class="col-9">
+                            <div class="input-group mb-3">
+                                <input type="number" class="form-control"  aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Convert</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <label for="">USD :</label>
+                        </div>
+                        <div class="col-9">
+                            <div class="input-group mb-3">
+                                <input type="number" class="form-control"  aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Convert</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <label for="">EURO :</label>
+                        </div>
+                        <div class="col-9">
+                            <div class="input-group mb-3">
+                                <input type="number" class="form-control"  aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Convert</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <label for="">GPB :</label>
+                        </div>
+                        <div class="col-9">
+                            <div class="input-group mb-3">
+                                <input type="number" class="form-control"  aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Convert</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </div>
+    </div>
 </body>
 </html>
