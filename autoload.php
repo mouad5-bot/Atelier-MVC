1b4e33b2ba62935ad5259f43fc30f146f88d944a
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-01-09 15:37:59 +0100

    add autoload file | required it

---
+++ autoload.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+spl_autoload_register('autoload');
+
+function autoload($class_name)
+{
+    $array_paths = array
+    (
+        'connection/',
+        'src/controller/',
+        'src/models/',
+        'src/views/'
+    );
+    $parts = explode('\\',$class_name);
+    $name = array_pop($parts);
+
+    foreach ($array_paths as $path) 
+    {
+        $file = sprintf($path.'%s.php',$name);
+        if(is_file($file)){
+            include_once $file ;
+        }
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once 'autoload.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
